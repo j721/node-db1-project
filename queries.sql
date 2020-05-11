320eb1c3492f5f063cfaaa6e99ead065fbbb583d
j721 <juliadvu@gmail.com>
2020-05-11 15:54:29 -0500

    5th sql statement done

--- queries.sql
+++ queries.sql
@@ -18,7 +18,12 @@ SELECT OrderDate FROM Orders LIMIT 10;
 
 SELECT * FROM [Customers] WHERE City = 'London' OR City = 'Madrid' OR Country = 'Brazil' 
 
--- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+-- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" 
+--the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+ 
+ INSERT INTO Customers (CustomerName,	ContactName,	Address,	City,	PostalCode,	Country)
+VALUES ("The Shire","Bilbo Baggins" ,"1 Hobbit-Hole","Bag End","111","Middle Earth")
+
 
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
