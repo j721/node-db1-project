ce32100c98667e36853b2f94c62e4b2715b4949e
j721 <juliadvu@gmail.com>
2020-05-11 16:27:15 -0500

    created accountsRouter and working on get request

---
+++ accounts/accountsRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// database access using knex
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+//GET
+  // get a list of accounts from the database
+  // select * from accounts;
+router.get("/", (req,res)=>{
+    db.select("*")
+    .from("accounts")
+    .then(accounts=>{
+        res.status(200).json({data: accounts})
+    })
+    .catch(error=>{
+        console.log(error)
+        res.status(500).json({message: error.message })
+    })
+})
